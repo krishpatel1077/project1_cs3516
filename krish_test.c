e0d4eb2359de5898bf038646e1b6f3ceeba18851
Krish Patel <2021kpatel@gmail.com>
2024-03-27 19:55:00 -0400

    added the datalink stuff

--- krish_test.c
+++ krish_test.c
@@ -1,3 +1,24 @@
 //There will be code here
+#include <stdio.h>
 #include <pcap/pcap.h>
-pcap_t *pcap_open_offline(char * fname, char * ebuf);
+
+int main() {
+    char ebuf[PCAP_ERRBUF_SIZE];
+    char fname = 'tcptext.txt';
+    pcap_t *pcap;
+    //open the input file
+    pcap = pcap_open_offline(fname, ebuf);
+    
+    if (pcap == NULL) {
+        fprintf(stderr, "Error opening file: %s\n", ebuf);
+        return 1;
+    }
+
+    int datalink = pcap_datalink(pcap);
+    if (datalink == DLT_EN10MB)
+        print("Data is from 10MB ethernet!");
+    else
+        print("Data is not from 10MB ethernet!");
+    pcap_close(pcap);
+    return 0;
+}
