0913626717bfde54ca32f2369b0cea4d639d228e
Krish Patel <2021kpatel@gmail.com>
2024-04-04 19:45:00 -0400

    added a thing

--- krish_test.c
+++ krish_test.c
@@ -6,18 +6,43 @@
 #include "/usr/include/net/ethernet.h"
 #include "/usr/include/netinet/tcp.h"
 #include "/usr/include/netinet/udp.h"
+#include <time.h>
+#include <stdint.h>
 
 int totalpackets = 0;
 int totallen = 0;
 void my_packet_handler(u_char *user_data, const struct pcap_pkthdr *pkthdr, const u_char *packet) {
+    // Set up time relevant variables
+    struct tm ltime;
+    char timestr[22];
+    time_t local_tv_sec;
     totalpackets++;
     totallen += pkthdr->len;
-    printf("Received a packet of length %d\n", pkthdr->len);
-    printf("Time Stamp of the packet is %ld.%06ld\n", pkthdr->ts.tv_sec, pkthdr->ts.tv_usec);
-
-    const u_char *ip_header;
-    const u_char *
     
+    // Time conversion + length
+    local_tv_sec = pkthdr->ts.tv_sec;
+    localtime_r(&local_tv_sec, &ltime);
+    strftime(timestr, sizeof timestr, "%Y-%m-%d %H:%M:%S", &ltime); // Update format string
+    printf("%s.%06ld len:%d \n", timestr, pkthdr->ts.tv_usec, pkthdr->len); // Include microseconds
+   
+
+    struct iphdr *iph; 
+    struct ether_header *ethdr;
+    struct ether_addr *ethr_src;
+    struct ether_addr *ethr_dest;
+
+    //get ethernet header + destination and source address
+    ethdr = (struct ether_header *)(packet);
+    ethr_src = (struct ether_addr *)ethdr->ether_shost;
+    ethr_dest = (struct ether_addr *)ethdr->ether_dhost;
+    
+    //IP addresses
+    iph = (struct iphdr *)(packet + 14);
+    char src_addr[INET_ADDRSTRLEN];
+    char dest_addr[INET_ADDRSTRLEN];
+    inet_ntop(AF_INET, &(iph->saddr), src_addr, INET_ADDRSTRLEN);
+    inet_ntop(AF_INET, &(iph->daddr), dest_addr, INET_ADDRSTRLEN);
+    printf("Src IP: %s, Dst IP: %s\n", src_addr, dest_addr);
 }
 
 int main() {
