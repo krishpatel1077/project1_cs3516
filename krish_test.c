fb23f97136462d8a6d68640064cda1717a34b3ad
Krish Patel <2021kpatel@gmail.com>
2024-04-06 10:34:52 -0400

    added AddressMap

--- krish_test.c
+++ krish_test.c
@@ -9,6 +9,81 @@
 #include "/usr/include/netinet/udp.h"
 #include <time.h>
 #include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+
+/*OUR STRUCTURE IS ADDRESSMAP: A general structure with two char arrays
+to hold any kind of network address for sender and receiver*/
+struct AddressMap {
+    char** sourceAddresses; // Pointer to an array of source addresses
+    char** destAddresses;   // Pointer to an array of destination addresses
+    int size;
+};
+
+struct AddressMap *initAddressMap() {
+    struct AddressMap *map = malloc(sizeof(struct AddressMap));
+    if (!map) {
+        perror("Memory allocation failed");
+        exit(EXIT_FAILURE);
+    }
+    map->sourceAddresses = NULL;
+    map->destAddresses = NULL;
+    map->size = 0;
+
+    return map;
+}
+
+// Function to add an address to the AddressMap
+//Identifier: 0 --> source, 1 --> destination
+void addAddress(struct AddressMap *a_map, char *address, int identifier) {
+    // Allocate memory for a new address
+    char *newAddress = strdup(address);
+    if (!newAddress) {
+        perror("Memory allocation failed");
+        exit(EXIT_FAILURE);
+    }
+    // identifier = 0 --> source address
+    if (identifier == 0) {
+        //allocate the memory for the source Address so it can store the values
+        a_map->sourceAddresses = realloc(a_map->sourceAddresses, (a_map->size + 1) * sizeof(char *));
+        if (!a_map->sourceAddresses) {
+            perror("Memory allocation failed");
+            exit(EXIT_FAILURE);
+        }
+        //add on the new address
+        a_map->sourceAddresses[a_map->size] = newAddress;
+    } 
+    //identigier = 1 --> desitination address
+    else if (identifier == 1) {
+        //allocate the memory for the dest address so it can store the values
+        a_map->destAddresses = realloc(a_map->destAddresses, (a_map->size + 1) * sizeof(char *));
+        if (!a_map->destAddresses) {
+            perror("Memory allocation failed");
+            exit(EXIT_FAILURE);
+        }
+        //store the new address
+        a_map->destAddresses[a_map->size] = newAddress;
+    } else {
+        printf("ERROR: BAD IDENTIFIER!\n");
+        free(newAddress); // Free memory allocated for the new address
+        return;
+    }
+    a_map->size++; // Increment the size of the map
+}
+
+// Function to print the addresses stored in the AddressMap
+void printAddresses(struct AddressMap *a_map) {
+    printf("Source Addresses:\n");
+    for (int i = 0; i < a_map->size; i++) {
+        printf("%s\n", a_map->sourceAddresses[i]);
+    }
+
+    printf("\nDestination Addresses:\n");
+    for (int i = 0; i < a_map->size; i++) {
+        printf("%s\n", a_map->destAddresses[i]);
+    }
+}
+
 
 int totalpackets = 0;
 int totallen = 0;
