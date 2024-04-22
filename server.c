acee7e96b3083e5e35c8b0d14d6d9b3636b745fb
Ceci Herriman <herrimanceci@gmail.com>
2024-04-21 20:07:08 -0400

    stuff

--- server.c
+++ server.c
@@ -15,7 +15,7 @@
  #include <sys/wait.h>
  #include <signal.h>
 
- #define PORT "  90" // the port users will be connecting to
+ #define PORT "3490" // the port users will be connecting to
  #define BACKLOG 10 // how many pending connections queue will hold
 
  void sigchld_handler(int s) {
@@ -108,25 +108,24 @@
         sin_size = sizeof their_addr;
         new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
         if (new_fd == -1) {
-        perror("accept");
-       continue;
-    }
-
-    inet_ntop(their_addr.ss_family,
-    get_in_addr((struct sockaddr *)&their_addr),
-    s, sizeof s);
-    printf("server: got connection from %s\n", s);
-
-    if (!fork()) { // this is the child process
-        close(sockfd); // child doesn't need the listener
-        if (send(new_fd, "Hello, world!", 13, 0) == -1) {
-            perror("send");
+            perror("accept");
+            continue;
         }
-        close(new_fd);
-        exit(0);
+
+        inet_ntop(their_addr.ss_family,
+        get_in_addr((struct sockaddr *)&their_addr), s, sizeof s);
+        printf("server: got connection from %s\n", s);
+
+        if (!fork()) { // this is the child process
+            close(sockfd); // child doesn't need the listener
+            if (send(new_fd, "Hello, world!", 13, 0) == -1) {
+             perror("send");
+            }
+            close(new_fd);
+            exit(0);
+        }
+        close(new_fd); // parent doesn't need this
     }
-    close(new_fd); // parent doesn't need this
-  }
  
   return 0;
 }
