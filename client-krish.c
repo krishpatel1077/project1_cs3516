663fab8e0c93241796db32869792c9a9a92ada06
Krish Patel <2021kpatel@gmail.com>
2024-04-30 15:04:14 -0400

    copied from the vm

---
+++ client-krish.c
@@ -0,0 +1,308 @@
+ #include <stdio.h>
+ #include <stdlib.h>
+ #include <unistd.h>
+ #include <errno.h>
+ #include <string.h>
+ #include <netdb.h>
+ #include <fcntl.h>
+ #include <sys/types.h>
+ #include <netinet/in.h>
+ #include <sys/socket.h>
+ #include <sys/stat.h>
+ #include <sys/types.h>
+ #include <sys/sendfile.h>
+
+ #include <arpa/inet.h>
+
+ #define DEFAULT_PORT "7159" // the port client will be connecting to
+ #define IP "10.23.36.1" //IP 
+ #define MAXDATASIZE 100 // max number of bytes we can get at once
+ char PORT[6];
+ ///starter code used from beej's guide to network programming
+
+
+ // get sockaddr, IPv4 or IPv6:
+ void *get_in_addr(struct sockaddr *sa) {
+    if (sa->sa_family == AF_INET) {
+     return &(((struct sockaddr_in*)sa)->sin_addr);
+    }
+
+ return &(((struct sockaddr_in6*)sa)->sin6_addr);
+ }
+
+ off_t get_file_size(char* file) {
+    struct stat buf; 
+
+    if(stat(file, &buf) == -1) {
+        //perror("Get file size:");
+        //exit(EXIT_FAILURE);
+        return 0; 
+    }
+
+    return buf.st_size; 
+ }
+
+ // Function to receive data from client and print it
+ void receive_and_print(int sockfd) {
+    //receive server return code 
+    int code; 
+    if (recv(sockfd, &code, sizeof(off_t), 0) == -1) {
+        perror("recv code");
+        exit(EXIT_FAILURE);
+    }
+    else {
+        if(code == 0) {
+            printf("0 - Success, the URL is being returned\n");
+        }
+        else if(code == 1) {
+            printf("1 - Failure, no URL is being returned\n");
+            return; 
+        }
+    }
+
+    // Receive the length of the data
+    off_t length;
+    if (recv(sockfd, &length, sizeof(off_t), 0) == -1)
+    {
+        perror("recv length");
+        exit(EXIT_FAILURE);
+    }
+
+    // Allocate memory for receiving buffer
+    char buffer[MAXDATASIZE];
+
+    // Open a file to write the received data
+    FILE *file = fopen("received_data.txt", "wb");
+    if (file == NULL)
+    {
+        perror("fopen");
+        exit(EXIT_FAILURE);
+    }
+
+    u_int32_t bytesReceivedSoFar = 0;
+    while (bytesReceivedSoFar < length)
+    {
+        // Receive data from the server
+        int bytesActuallyReceived = recv(sockfd, buffer, MAXDATASIZE, 0);
+
+        if (bytesActuallyReceived == -1) {
+            perror("recv data");
+            exit(EXIT_FAILURE);
+        }
+
+        // Write received data to the file
+        fwrite(buffer, 1, bytesActuallyReceived, file);
+
+        bytesReceivedSoFar += bytesActuallyReceived;
+    }
+
+    printf("client: received %d bytes of converted data from server\n", bytesReceivedSoFar);
+
+    // Close the file
+    fclose(file);
+
+    FILE *fileRead = fopen("received_data.txt", "rb");
+    char string[255];
+
+    if(fileRead == NULL) {
+        printf("null file");
+    }
+    
+    printf("QR code result:\n\n");
+
+    while (fgets(string, 255, fileRead) != NULL) {
+        printf("%s", string); 
+    }
+
+    fclose(fileRead);
+
+}
+
+void send_file_data(char* name, int sockfd) {
+    off_t fileSize; 
+    FILE *qrFile;
+
+    //find file size given the file descriptor
+    fileSize = get_file_size(name);
+    
+    printf("Your inputted file size is %ld\n", fileSize);
+
+    //send file data
+    qrFile = fopen(name, "r");
+    char sendingBuf [fileSize];
+    int sendingSize; 
+
+    bzero(sendingBuf, fileSize);
+
+    if (qrFile == NULL) {
+        perror("opening file");
+    }
+
+    //send file size first
+    if(send(sockfd, &fileSize, sizeof(off_t), 0) == -1) {
+        perror("sending file size value");
+    }
+       
+    printf("client: sending the file size %ld\n", fileSize);
+
+    //loop to send actual data 
+    while((sendingSize = fread(sendingBuf, 1, fileSize, qrFile)) > 0) {
+        if(send(sockfd, sendingBuf, sendingSize, 0) == -1) {
+            perror("sending file");
+        }
+       
+        printf("client: sent %d bytes of inputted file to server\n", sendingSize);
+        bzero(sendingBuf, fileSize);
+    }
+}
+
+ int main(int argc, char *argv[]) {
+    int sockfd, numbytes;
+    char buf[MAXDATASIZE];
+    struct addrinfo hints, *servinfo, *p;
+    int rv;
+    char s[INET6_ADDRSTRLEN];
+    int qrFD;
+    off_t sendingOffset; 
+    int sentBytes; 
+    off_t remainingData;
+    int fd; 
+    
+   /* if (argc != 2) {
+        fprintf(stderr,"usage: PORT #\n");
+        exit(1);
+    }
+   */
+    strcpy(PORT, DEFAULT_PORT);
+    if (strcmp(argv[0], "PORT") == 0)
+        strcpy(PORT, argv[1]);    
+
+    memset(&hints, 0, sizeof hints); hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+
+    if ((rv = getaddrinfo(IP, PORT, &hints, &servinfo)) != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+        return 1;
+    }
+
+    // loop through all the results and connect to the first we can
+    for(p = servinfo; p != NULL; p = p->ai_next) {
+        if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
+            perror("client: socket");
+            continue;
+        }   
+
+        if (connect(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+            close(sockfd);
+            perror("client: connect");
+            continue;
+        }
+
+        break;
+    }
+
+    if (p == NULL) {
+        fprintf(stderr, "client: failed to connect\n");
+        return 2;
+    }
+
+    inet_ntop(p->ai_family, get_in_addr((struct sockaddr *)p->ai_addr), s, sizeof s);
+    printf("client: connecting to %s\n", s);
+
+    freeaddrinfo(servinfo); // all done with this structure
+
+    //receive first message from server about command line commands
+    if ((numbytes = recv(sockfd, buf, MAXDATASIZE-1, 0)) == -1) {
+        perror("recv");
+        exit(1);
+    }
+ 
+    buf[numbytes] = '\0';
+    printf("client: received '%s'\n",buf);
+
+    //respond to received message from server
+    if (send(sockfd, "Hello, server!\n", 14, 0) == -1) {
+        perror("sending message");
+        exit(1); 
+    }
+
+    int doClose = 0; 
+    while(doClose == 0) {
+        printf("in loop");
+        //get command line input
+        char input[100];
+        int n = scanf("%s", input);
+        if(n != 1) {
+            memset(input, 0, strlen(input));
+        }
+        printf("hhh%s\n", input);
+
+        if(strcmp(input, "close") == 0) {
+            //send that the client wants to close (3)
+            if (send(sockfd, "3333", 4, 0) == -1) {
+                perror("send");
+            }
+
+            if ((numbytes = recv(sockfd, buf, 32, 0)) == -1) {
+                perror("recv");
+                exit(1);
+            }
+
+            buf[numbytes] = '\0';
+
+            printf("'%s'\n",buf);
+
+            close(sockfd); 
+            doClose = 1;
+            exit(0); 
+            memset(input, 0, strlen(input));
+        }
+        else if (strcmp(input, "shutdown") == 0) {
+            //send that the client wants to shutdown (1)
+            if (send(sockfd, "1111", 4, 0) == -1) {
+                perror("send");
+            }
+            memset(input, 0, strlen(input));
+            doClose = 1;
+        }
+        else if(strlen(input) > 0) {
+            //assume png is inputted, and send it if file size != 0 (2)
+            off_t fileSize; 
+            fileSize = get_file_size(input);
+
+            if(fileSize == 0) {
+                printf("1 - Failure, no URL is being returned\n"); 
+            }
+
+            else {
+                if (send(sockfd, "2222", 4, 0) == -1) {
+                    perror("send");
+                }
+
+                send_file_data(input, sockfd);
+                memset(input, 0, strlen(input));
+
+                receive_and_print(sockfd);
+            }
+        }
+        
+            //check for timeout
+
+            printf("check for timeout");
+            if ((numbytes = recv(sockfd, buf, 32, 0)) == -1) {
+                perror("recv");
+                exit(1);
+            }
+
+            buf[numbytes] = '\0';
+
+            printf("'%s'\n",buf);
+            close(sockfd); 
+            doClose = 1; 
+        
+    }
+
+    close(sockfd);
+
+    return 0;
+}
---
+++ server-krish.c
@@ -0,0 +1,419 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <signal.h>
+#include <time.h> // for time functions
+
+#define DEFAULT_PORT "2012" // the port users will be connecting to
+#define BACKLOG 10 // how many pending connections queue will hold
+#define MAXDATASIZE 100 // max number of bytes we can get at once
+
+///starter code used from beej's guide to network programming
+
+//global variables for options 
+char PORT[6];
+int rate_limit = 3;
+int rate_limit_time = 60; 
+int max_users = 3; 
+int timeout = 80;
+
+// Function to log administrative activities
+void log_activity(const char *action, const char *client_ip) {
+    time_t current_time;
+    struct tm *time_info;
+    char time_str[20];
+    FILE *log_file;
+
+    // Get current time
+    time(&current_time);
+    time_info = localtime(&current_time);
+    strftime(time_str, sizeof(time_str), "%Y-%m-%d %H:%M:%S", time_info);
+
+    // Open the log file in append mode, create if it doesn't exist
+    log_file = fopen("admin_log.txt", "a+");
+    if (log_file == NULL) {
+        perror("Error opening log file");
+        return;
+    }
+
+    // Write log entry to file
+    fprintf(log_file, "----\n%s %s %s\n", time_str, action, client_ip);
+    fclose(log_file);
+}
+
+// get sockaddr, IPv4 or IPv6:
+void *get_in_addr(struct sockaddr *sa) {
+    if (sa->sa_family == AF_INET) {
+        return &(((struct sockaddr_in*)sa)->sin_addr);
+    }
+    return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
+
+// SIGCHLD handler to reap zombie processes
+void sigchld_handler(int s) {
+    // Wait for all dead processes
+    while (waitpid(-1, NULL, WNOHANG) > 0);
+}
+
+// get size of inputted file
+off_t get_file_size(char* file) {
+    struct stat buf; 
+
+    if(stat(file, &buf) == -1) {
+        perror("Get file size:");
+        exit(EXIT_FAILURE);
+    }
+
+    return buf.st_size; 
+ }
+
+// Function to receive data from client and write it to a file
+FILE* receive_and_write(int sockfd) {
+    FILE* file; 
+
+    // Receive the length of the data
+    off_t length;
+    if (recv(sockfd, &length, sizeof(off_t), 0) == -1) {
+        perror("recv length");
+        file = NULL; 
+        return file; 
+    }
+
+    //check to see if length is greater than our security limit of 7000
+    if(length > 800000) {
+        file = NULL; 
+        return file;
+    }
+
+    // Allocate memory for receiving buffer
+    char buffer[MAXDATASIZE];
+
+    // Open a file to write the received data
+    file = fopen("received_data.png", "wb");
+    if (file == NULL) {
+        perror("fopen");
+        return file; 
+    }
+
+    u_int32_t bytesReceivedSoFar = 0;
+    while (bytesReceivedSoFar < length) {
+        // Receive data from the client
+        int bytesActuallyReceived = recv(sockfd, buffer, MAXDATASIZE, 0);
+        if (bytesActuallyReceived == -1) {
+            perror("recv data");
+            exit(EXIT_FAILURE);
+        }
+
+        // Write received data to the file
+        fwrite(buffer, 1, bytesActuallyReceived, file);
+
+        bytesReceivedSoFar += bytesActuallyReceived;
+    }
+    printf("server: received %d bytes of sent file\n", bytesReceivedSoFar);
+
+    // Close the file
+    fclose(file);
+
+    return file; 
+}
+
+//Function to accept file contents, convert to url, and send back
+void do_url(int new_fd) {
+    // Write received data to a file
+    FILE* qrFile; 
+    qrFile = receive_and_write(new_fd);
+
+    if(qrFile == NULL) {
+        //if null, program could not receive and write, so send return code 1
+        int one = 1;
+        if(send(new_fd, &one, sizeof(int), 0) == -1) {
+            perror("sending return code");
+        }
+        else {
+            printf("server: sending return code 1 - Failure\n");
+        }
+    }
+
+    else {
+
+    //convert received_data.png to QR code, print results to QRresult.txt 
+    system("java -cp javase.jar:core.jar com.google.zxing.client.j2se.CommandLineRunner received_data.png > QRresult.txt");
+            
+    //send url result, size, and return code 
+    off_t fileSize; 
+    fileSize = get_file_size("QRresult.txt");
+
+    FILE * dataFile;
+    dataFile = fopen("QRresult.txt", "r");
+
+    char sendingBuf [fileSize];
+    char sizeBuf [sizeof(off_t)];
+
+    int sendingSize; 
+    bzero(sendingBuf, fileSize);
+
+    if (dataFile == NULL) {
+        //send failure code 1
+        int one = 1;
+        if(send(new_fd, &one, sizeof(int), 0) == -1) {
+            perror("sending return code");
+        }
+    }
+
+    //send return code failure - 1 if url not found
+    if(fileSize == 0) {
+        int one = 1;
+        if(send(new_fd, &one, sizeof(int), 0) == -1) {
+            perror("sending return code");
+        }
+        else {
+            printf("server: sending return code 1 - Failure\n");
+        }
+    }
+
+    //if url size > 0, continue
+    else {
+    
+    //send return code success - 0 
+    int zero = 0;
+        if(send(new_fd, &zero, sizeof(int), 0) == -1) {
+            perror("sending return code");
+        }
+        else {
+            printf("server: sending return code 0 - success\n");
+        }
+
+    //send url size first
+    if(send(new_fd, &fileSize, sizeof(off_t), 0) == -1) {
+        perror("sending url size value");
+    }
+       
+    printf("server: sending the url size %ld\n", fileSize);
+
+    //loop to send actual data 
+    while((sendingSize = fread(sendingBuf, 1, fileSize, dataFile)) > 0) {
+        if(send(new_fd, sendingBuf, sendingSize, 0) == -1) {
+                perror("sending file");
+        }
+       
+        printf("server: sent %d bytes of url to client\n", sendingSize);
+        bzero(sendingBuf, fileSize);
+    }
+    }
+    }
+}
+
+int main(int argc, char* argv[]) {
+    int sockfd, new_fd, numbytes; // listen on sock_fd, new connection on new_fd
+    struct addrinfo hints, *servinfo, *p;
+    struct sockaddr_storage their_addr; // connector's address information
+    socklen_t sin_size;
+    struct sigaction sa;
+    int yes = 1;
+    char s[INET6_ADDRSTRLEN];
+    char buf[MAXDATASIZE];
+    int rv;
+    
+
+    //parse command line arguments for options 
+    strcpy(PORT, DEFAULT_PORT); //intialize port with defualt val
+    for(int i = 0; i < argc; i++) {
+        if(strcmp(argv[i], "PORT") == 0) {
+            strcpy(PORT, argv[i + 1]); //change port to arg value 
+        }
+        else if(strcmp(argv[i], "RATE") == 0) {
+            if(i + 2 < argc) {
+                rate_limit = atoi(argv[i + 1]);
+                rate_limit_time = atoi(argv[i + 2]);
+            }
+        }
+        else if(strcmp(argv[i], "MAX USERS") == 0) {
+            if(i + 1 < argc) {
+                max_users = atoi(argv[i + 1]);
+            }
+        }
+        else if(strcmp(argv[i], "TIME OUT") == 0) {
+            if(i + 1 < argc) {
+                timeout = atoi(argv[i + 1]);
+            }
+        }
+    }
+
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE; // use my IP
+
+    if ((rv = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+        return 1;
+    }
+
+    // Loop through all the results and bind to the first we can
+    for (p = servinfo; p != NULL; p = p->ai_next) {
+        if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
+            perror("server: socket");
+            continue;
+        }
+
+        if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
+            perror("setsockopt");
+            exit(1);
+        }
+
+        if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+            close(sockfd);
+            perror("server: bind");
+            continue;
+        }
+
+        break;
+    }
+
+    freeaddrinfo(servinfo); // all done with this structure
+
+    if (p == NULL) {
+        fprintf(stderr, "server: failed to bind\n");
+        exit(1);
+    }
+
+    if (listen(sockfd, BACKLOG) == -1) {
+        perror("listen");
+        exit(1);
+    }
+
+    sa.sa_handler = sigchld_handler; // reap all dead processes
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = SA_RESTART;
+
+    if (sigaction(SIGCHLD, &sa, NULL) == -1) {
+        perror("sigaction");
+        exit(1);
+    }
+
+    printf("server: waiting for connections...\n");
+
+    while (1) { // main accept() loop
+        sin_size = sizeof their_addr;
+
+        // New connection established
+        new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
+        if (new_fd == -1) {
+            //perror("accept");
+            continue;
+        }
+
+        // Print client's address
+        inet_ntop(their_addr.ss_family, get_in_addr((struct sockaddr *)&their_addr), s, sizeof s);
+        printf("server: got connection from %s\n", s);
+
+        // Log connection
+        log_activity("Connection established", s);
+
+        if (!fork()) { // this is the child process
+            close(sockfd); // child doesn't need the listener
+
+            // Send message to client
+            if (send(new_fd, "Enter 'close' to disconnect, 'shutdown' to turn off server, or a qr code filename to get URL: \n", 98, 0) == -1) {
+                perror("send");
+            }
+
+            if ((numbytes = recv(new_fd, buf, 14, 0)) == -1) {
+                perror("recv");
+                exit(1);
+            }
+ 
+            buf[numbytes] = '\0';
+
+            printf("server: received '%s'\n",buf);
+
+            //start inactivity timer (resets when a new command line input is received from client
+            time_t startTime = time(NULL); 
+
+            int doClose = 0;
+            while(doClose == 0) { //change to have time while loop --> deal w/ one of the two after loop
+
+                //check for timeout (10 used for testing purposes)
+                //printf("%ld\n", time(NULL) - startTime);
+                if(time(NULL) - startTime > timeout) {
+                    //timeout time reached
+
+                    //report timeout using return code 
+                    printf("timeout reached\n");
+
+                    //close connection 
+                    close(new_fd);
+
+                    // Log disconnection
+                    log_activity("Connection closed", s);
+
+                    doClose = 1; 
+
+                }        
+                
+                //get command line input (3333 - close, 1111 - shutdown, 2222 - file)
+                int input;
+                char numBuf[2];
+                numBuf[1] = '\0';
+
+                if ((numbytes = recv(new_fd, numBuf, 4, 0)) == -1) {
+                    //perror("recv input");
+                    //exit(1);
+                    //printf("nothing received\n");
+                    input = 4; 
+                }
+                else {
+                    input = atoi(numBuf);
+                    startTime = time(NULL); 
+                    //printf("input: %d, %s\n", input, numBuf);
+                }
+
+                // if input is close, do close function
+                if(input == 3333) {
+                    //do close function
+
+                    //send server code 2 and message 
+                    if (send(new_fd, "2 - Connection is being closed\n", 32, 0) == -1) {
+                        perror("send");
+                    }
+
+                    //close connection 
+                    close(new_fd); 
+                    doClose = 1; 
+
+                    printf("Server: closed connection with %s\n", s);
+
+                }
+
+                //if input is shutdown, do shutdown function 
+                if(input == 1111) {
+                    printf("do shutdown function\n");
+                    //do shutdown function
+                }
+
+                //if input is a file, find url
+                if(input == 2222) {
+                    //printf("do url function\n");
+                    //do url function
+                    do_url(new_fd);
+                }
+
+                input = 4; 
+                bzero(numBuf, 2);
+                
+            }
+            close(new_fd); // parent doesn't need this
+        }
+        //printf("here");
+
+    }
+}
